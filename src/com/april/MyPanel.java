959456699d2143c36161ef9f3c4e657a30363e09
Roman <44396617+RomanDevyatov@users.noreply.github.com>
2021-02-15 12:16:16 +0300

    Update MyPanel.java

--- src/com/april/MyPanel.java
+++ src/com/april/MyPanel.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 
 import javax.imageio.ImageIO;
@@ -16,17 +18,47 @@ import javax.swing.Timer;
 
 public class MyPanel extends JPanel {
     public Game game;
-    private Timer timer;
+    private Timer tmDraw, tmUpdate;
     private Image
-            fon,
-            telo,
-            golova,
-            ob,
-            endg;
+            	fon,
+            	telo,
+            	golova,
+            	ob,
+            	endg;
     private JLabel lb;
     private JButton btn1, btn2;
+    private MyPanel pan;//
+    
+    private class MyKey implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			
+			if (key == KeyEvent.VK_LEFT) {
+				game.setNapr(0);
+			} else if (key == KeyEvent.VK_UP) {
+				game.setNapr(1);
+			} else if (key == KeyEvent.VK_RIGHT) {
+				game.setNapr(2);
+			} else if (key == KeyEvent.VK_DOWN) {
+				game.setNapr(3);
+			}			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}
+    	
+    }
 
     public MyPanel() {
+    	pan = this;//
+    	this.addKeyListener(new MyKey());
+    	this.setFocusable(true);
+    	
         try {
             this.fon = ImageIO.read(new File("C:\\Users\\r.devyatov\\Downloads\\Zmeika\\fon.png"));
             this.telo = ImageIO.read(new File("C:\\Users\\r.devyatov\\Downloads\\Zmeika\\telo.png"));
@@ -37,17 +69,29 @@ public class MyPanel extends JPanel {
             System.out.println("Файл не прочитался");
         }
 
-        game = new Game();
-        game.start();
-
-        timer = new Timer(10, new ActionListener( ) {
+        this.game = new Game();
+        this.game.start();
+        
+        tmDraw = new Timer(10, new ActionListener( ) {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 repaint();
             }
         });
-        timer.start();
+        tmDraw.start();
+        
+        tmUpdate = new Timer(100, new ActionListener( ) {
+        	@Override
+            public void actionPerformed(ActionEvent e) {                
+                if (game.getEndg() == false) {
+                	game.peremGolova();
+                }
+        		lb.setText("Счёт: " + game.getKol());
+            }
+        });
+        tmUpdate.start();
+        
         setLayout(null);
 
         lb = new JLabel("Счет: 0");
@@ -64,6 +108,9 @@ public class MyPanel extends JPanel {
         btn1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 game.start();
+                btn1.setFocusable(false);//
+                btn2.setFocusable(false);//
+                pan.setFocusable(true);//
             }
         });
         add(btn1);
@@ -85,15 +132,17 @@ public class MyPanel extends JPanel {
         super.paintComponent(gr);
 
         gr.drawImage(fon, 0, 0, 800, 650, null);
-
+        
         for (int i = 0; i < 30; i++) {
             for (int k = 0; k < 30; k++) {
                 if (game.getMasByXY(k, i) != 0) {
                     if (game.getMasByXY(k, i) == 1) {
                         gr.drawImage(golova, 10 + k * 20, 10 + i * 20, 20, 20, null);
                     } else if (game.getMasByXY(k, i) == -1) {
-                        gr.drawImage(ob, 10 + k * 20, 10 + i * 20, 20, 20, null);
-                    }
+                        gr.drawImage(ob, 10 + k * 20, 10 + i * 20, 20, 20, null);}
+//                    } else if (game.getMasByXY(k, i) >= 2) {
+//                    	gr.drawImage(telo, 10 + k * 20, 10 + i * 20, 20, 20, null);
+//                    }
                 }
             }
         }
@@ -103,10 +152,16 @@ public class MyPanel extends JPanel {
             gr.drawLine(10 + i * 20, 10, 10 + i * 20, 610);
             gr.drawLine(10, 10 + i * 20, 610, 10 + i * 20);
         }
-
+        
         //i = 0: gr.drawLine(10, 10, 10, 610);
         //i = 1: gr.drawLine(30, 10, 30, 610);
         //i = 2: gr.drawLine(50, 10, 50, 610);
+        
+//        if (game.getEndg() == true) {
+//        	gr.drawImage(endg, 250, 200, 200, 100, null);
+//        }
+
+        
 
 
     }
